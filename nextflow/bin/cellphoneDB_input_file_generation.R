a359a4dc970aa540051a381f293c26def691b915
Oscar Wright <oscar-wright@mail.com>
2022-04-22 18:54:14 -0400

    cellphoneDB file prep done

--- nextflow/bin/cellphoneDB_input_file_generation.R
+++ nextflow/bin/cellphoneDB_input_file_generation.R
@@ -5,119 +5,59 @@ library(argparse)
 
 parser <- ArgumentParser(description='An executible script to prepare files to run cellphoneDB in a patient-wise manner.')
 
-parser$add_argument("-d", "--data_directory", type="character", dest="data_directory", help="Provide the full path to directory in which all patient count data is stored.")
+parser$add_argument("-so", "--seurat_object", type="character", dest="seurat_object", help="Provide the full path to the seurat object for ehich you will run cellphoneDB on.")
+parser$add_argument("-o", "--output_directory", type="character", dest="output_directory", help="Provide the full path to directory in which to store the putput of this script.")
+parser$add_argument("-id", "--sample_id_meta", type="character", dest="sample_id_meta", help="Provide the metadata column of the seurat object that contains the sampleID information.")
+parser$add_argument("-l", "--celltype_label_meta", type="character", dest="celltype_label_meta", help="Provide the metadata column of the seurat object that contains the celltype/compartment information. This is the levels at which we will compare cell interactions.")
 
 args <- parser$parse_args()
-data_directory <- args$data_directory
+seurat_object <- args$data_directory
+output_directory <- args$output_directory
+sample_id_meta <- args$sample_id_meta
+celltype_label_meta <- args$celltype_label_meta
 
 #! del these
-data_directory <- '/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/data/PRIV_GITHUB/SCGA/nextflow/bin/data'
+seurat_object <- '/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/data/adj_normal_subset/for_manu/draft3/highlevel/highevel_with_luad_matched_labels.Rda'
+sample_id_meta <- 'SampleID'
+celltype_label_meta <- 'luad_label_match'
+output_directory <- '/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/data/adj_normal_subset/for_manu/draft3/highlevel/'
 #!
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 library(ggplot2)
 library(Seurat)
 library(dplyr)
 library(Matrix)
-
-data.path <- "/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/data/pAdeno_early_naive_subset/htan_msk_addition/draft2/highlevel/v12.highlevel.Rda"
-proj.path <- "/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/cellphonedb/new_age"
-
-
-# source(paste0(user.path, "/R_utils/plotutils.R"))
-# source(paste0(user.path, "/R_utils/seuratutils.R"))
-# source(paste0(user.path, "/R_utils/seuratutilsV3.R"))
-# source(paste0(user.path, "/R_utils/color.R"))
-
-# user.path <- "/ahg/regevdata/projects/ICA_Lung/Will"
-# #Genesets
-# cc.genes <- readLines(con = paste0(user.path, "/genelists/regev_lab_cell_cycle_genes.txt"))
-# s.genes <- cc.genes[1:43]
-# g2m.genes <- cc.genes[44:97]
-
-# figures.dir <- paste0(proj.path, "/Results/10x_All_190615/Allv9/")
-# load(file = paste0(figures.dir, "all.merge.new.cci.nodoublets.v2.Rda"))
-load(file = data.path)
-# all.bypatient <- SplitObject(all, split.by = "orig.identSec")
-# split by patientID
-all.bypatient <- SplitObject(v12.celltype, split.by = "sampleID")
-
-ls()
-typeof(all.bypatient)
-
 library(future.apply)
 
-cpdb.output.path <- paste0(proj.path, "/cell_cell_int_padeno_highlvl/")
-dir.create(file.path(cpdb.output.path), showWarnings = FALSE)
 
-options(future.globals.maxSize = 48000 * 1024^2)
+v12.celltype <- get(load(file = seurat_object))
 
+# split by sample ID
+all.bypatient <- SplitObject(v12.celltype, split.by = sample_id_meta)
 
-
-#*############
-saveRDS(all.bypatient, file = paste0(proj.path, "/all.bypatient.rds"))
-
-sample.names <- list.files("/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/cellphonedb/")
-sample.names <- sample.names[!(sample.names %in% c("draft_results","smoking_high_low.csv", "Differential_analysis", "new_Differential_analysis","EGFR_mut_wt.csv", "new_age"))]
-old_samples <- vector("numeric")
-for (i in 1:length(all.bypatient)){
-  if (unique(all.bypatient[[i]]$sampleID) %in% sample.names){
-    print(unique(all.bypatient[[i]]$sampleID))
-    old_samples <- append(old_samples, i)
-  }
+cpdb.output.path <- paste0(output_directory, "/cell_cell_int_padeno_highlvl/")
+if (!file.exists(file.path(cpdb.output.path))){
+  dir.create(file.path(cpdb.output.path), showWarnings = FALSE)
 }
-print(sample.names)
 
-# Only want to run cellphonedb on samples we haven't run before.
-new.bypatient <- all.bypatient[-old_samples]
-saveRDS(new.bypatient, file = paste0(proj.path, "/new.bypatient.rds"))
-
-
-#*############
-
-
-data.path <- "/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/data/pAdeno_early_naive_subset/htan_msk_addition/draft2/highlevel/v12.highlevel.Rda"
-proj.path <- "/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/cellphonedb/new_age"
-
-new.bypatient <- readRDS(paste0(proj.path, "/new.bypatient.rds"))
-
-library(future.apply)
-cpdb.output.path <- paste0(proj.path, "/cell_cell_int_padeno_highlvl/")
 options(future.globals.maxSize = 48000 * 1024^2)
 
-sub_new.bypatient <- new.bypatient[31:35] #! ... EDIT THIS
 
-
-lapply(sub_new.bypatient, function(x){
-  sampleid <- unique(x$sampleID)
-  print(sampleid)
-  print(paste0("Creating directory:", paste0(cpdb.output.path, sampleid)))
+lapply(all.bypatient, function(x){
+  sampleid <- unique(x@meta.data[,sample_id_meta])
+  #print(sampleid)
+  #print(paste0("Creating directory:", paste0(cpdb.output.path, sampleid)))
   dir.create(file.path(cpdb.output.path, sampleid), showWarnings = FALSE)
   counts <- x[["RNA"]]@counts
   counts.norm <- future_apply(counts, 2, function(x) (x/sum(x))*10000) # this is recommended by the cellphonedb paper
-  print('2')
+  #print('2')
   write.table(counts.norm, paste0(cpdb.output.path, sampleid, "/", sampleid, "_counts_highlvl.txt"), sep="\t", quote=F)
-  print(paste0(cpdb.output.path, sampleid, "/", sampleid, "_counts_highlvl.txt"))
+  #print(paste0(cpdb.output.path, sampleid, "/", sampleid, "_counts_highlvl.txt"))
   meta <- x@meta.data
-  meta <- cbind(rownames(meta), meta[,'celltype_highlevel', drop=F])
+  meta <- cbind(rownames(meta), meta[,celltype_label_meta, drop=F])
   meta[is.na(meta[,2]),2]<-'Unassigned'
   colnames(meta)<-c('cell','annotation')
   write.table(meta, paste0(cpdb.output.path, sampleid, "/", sampleid, "_meta_highlvl.txt"), sep="\t", quote=F, row.names=F)
-  print(paste0(cpdb.output.path, sampleid, "/", sampleid, "_meta_highlvl.txt"))
+  #print(paste0(cpdb.output.path, sampleid, "/", sampleid, "_meta_highlvl.txt"))
 })
